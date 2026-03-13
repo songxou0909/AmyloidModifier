abe034279359cfb450ebdae06a238037d9d0d35b
Songx <songxou0909@gmail.com>
2026-03-14 03:24:50 +0800

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,12 +1,10 @@
 from chimerax.core.toolshed import BundleAPI
 
 class _MyAPI(BundleAPI):
-    # This explicitly tells ChimeraX to use the modern argument structure
     api_version = 1
     
     @staticmethod
     def start_tool(session, bi, ti):
-        # Imports the tool class from your tool.py file and launches it
         from .tool import PDBModifierTool
         return PDBModifierTool(session, ti.name)
 
